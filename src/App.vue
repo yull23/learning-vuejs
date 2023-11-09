69c17f08bbe5a63b70d0118428f1ed45d61dc58c
Yull Timoteo <yull.timoteo.z@uni.pe>
2023-11-09 17:58:17 -0500

    Using v-for for two loops

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,33 @@
 <script setup>
-const name = "Vue 3";
-const active = true;
 const arrayFruts = [
   {
     name: "Manzana",
     price: "$1.00",
     description: "Una manzana",
+    count: 5,
   },
   {
     name: "Pera",
     price: "$2.00",
     description: "Una pera",
+    count: 10,
   },
   {
     name: "Naranja",
     price: "$3.00",
     description: "Una naranja",
+    count: 8,
   },
 ];
 const number = 2
 
 </script>
 <template>
-  <h1>Hola</h1>
-  <p>{{ name }}</p>
-
-  <div>
-    <ul>
-      <li v-for="element in arrayFruts">
-        <p>{{ element.name }}</p>
-        <p>{{ element.price }}</p>
-        <p>{{ element.description }}</p>
-      </li>
-    </ul>
-  </div>
-  <div>
-    <p v-if="number > 5">Number is greater than 5</p>
-    <p v-else-if="number == 5">Number is equal to 5</p>
-    <p v-else>Number is less than 5</p>
-  </div>
-  <div>
-    <h2 v-if="active">v-if en acción</h2>
-    <h2 v-else>v-else en acción</h2>
-  </div>
+  <p>Hola</p>
+  <ul v-for="(objectFruit, index) in arrayFruts" :key="objectFruit.name">
+    <p>Frut: {{ objectFruit.name }}</p>
+    <li v-for="(value, key) in objectFruit" :key="key">
+      {{ key }} : {{ value }}
+    </li>
+  </ul>
 </template>
-<style></style>
-
