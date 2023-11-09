04c12d330c1fc0a8c1cdc5978bfc14e9100aba1c
Yull Timoteo <yull.timoteo.z@uni.pe>
2023-11-09 17:29:46 -0500

    Using v-for to iterate over an object

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,36 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+const name = "Vue 3";
+const arrayFruts = [
+  {
+    name: "Manzana",
+    price: "$1.00",
+    description: "Una manzana",
+  },
+  {
+    name: "Pera",
+    price: "$2.00",
+    description: "Una pera",
+  },
+  {
+    name: "Naranja",
+    price: "$3.00",
+    description: "Una naranja",
+  },
+];
 </script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
-</template>
+  <h1>Hola</h1>
+  <p>{{ name }}</p>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+  <div>
+    <ul>
+      <li v-for="element in arrayFruts">
+        <p>{{ element.name }}</p>
+        <p>{{ element.price }}</p>
+        <p>{{ element.description }}</p>
+      </li>
+    </ul>
+  </div>
+</template>
+<style></style>
+
